29ba6611eeba4b4052025a5d1cdffb74bca10321
timob <timo.bloch.2008@gmail.com>
2024-03-13 14:11:22 +0100

    lolloll

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-test = 0
+class Subjects:
+    def __init__(self, marks):
+        self.marks = marks
+
+
+def load_data():
+    subjects = []
+    names = []
+    tot_marks = []
+
+    with open("safefile.txt") as file:
+
+        for line in file:
+            name = line[:line.index(":")]
+            marks = line[line.index(":") + 1:]
+
+            marks = marks.split(",")
+
+            tot_marks.append(marks)
+            names.append(name)
+
+    return names, tot_marks
+
+if __name__ == '__main__':
+    names, marks = load_data()
+    print(names, marks)
