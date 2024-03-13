82522dcc12b9dcdd91352249360467c47708e690
B1tcr4ck <timo.bloch.2008@gmail.com>
2024-03-13 20:49:01 +0100

    lööli

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 class Subjects:
-    def __init__(self, name, marks):
+    def __init__(self, name, ex_names, ex_marks):
         self.name = name
-        self.marks = marks
+        self.ex_marks = ex_marks
+        self.ex_names = ex_names
+        self.average = sum(ex_marks)/len(ex_marks)
+
 
 
 def load_data():
     subjects = []
     names = []
-    tot_marks = []
 
     with open("safefile.txt") as file:
 
         for line in file:
-            name = line[:line.index(":")]
-            marks = line[line.index(":") + 1:]
 
+            ex_names = []
+            ex_marks = []
+
+            name = line[:line.index("\\")]
+            marks = line[line.index("\\"):-1]
             marks = marks.split(",")
-
-            tot_marks.append(marks)
             names.append(name)
 
-    for subject in names:
-        subject = Subjects(subject, tot_marks[names.index(subject)])
-        subjects.append(subject)
+            for mark in marks:
+                mark = mark.split(":")
+                print(mark)
+                ex_names.append(mark[0])
+                ex_marks.append(int(mark[1]))
+
+
+            name = Subjects(name, ex_names, ex_marks)
+            subjects.append(name)
 
     return subjects
-def do_nothing():
+
+
+def run(subjects):
     pass
+
 
 if __name__ == '__main__':
     subjects = load_data()
-    print(subjects[0].name, subjects[0].marks)
+    run(subjects)
